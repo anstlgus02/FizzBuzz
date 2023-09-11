a20024298e132a38b4932487b0c350308b7cd2ff
anstlgus02 <chloe.moon@mail.utoronto.ca>
2023-09-11 13:04:52 -0400

    Task 4 <=

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,7 +1,9 @@
 public class Multiples {
     public static void main(String[] args) {
         int add = 0;
-        for (int i = 1; i < 1000; i++) {
+
+        for (int i = 1; i <= 1000; i++) {
+
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
